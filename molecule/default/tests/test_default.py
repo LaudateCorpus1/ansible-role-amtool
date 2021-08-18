287dd7f5cf75e397d6acef271055d9a05a29d04a
nre <nre@ableton.com>
2021-08-18 18:22:48 +0200

    Add molecule tests to role

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+from jenkins import Jenkins
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_amtool_installed(host):
+    test_bin_dir = host.file('/tmp/bin')
+    test_amtool = host.file('/tmp/bin/amtool')
+
+    assert test_bin_dir.is_directory
+    assert test_amtool.is_file
+    assert test_amtool.mode == 0o0755
